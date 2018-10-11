33937402b5a0641f4433553d3201ce0ba2ec9a11
SlawomirJablonski <forest19@interia.pl>
2018-10-11 17:33:11 +0200

    added ex2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6.  Wprowadzenie to testowania oprogramowania");
+        /*
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        */
+
+        //test for Calculator.add
+
+        Calculator calculatorAdd = new Calculator();
+        int sum = calculatorAdd.add(0,0);
+        if (sum == 0){
+            System.out.println(" add operation for 0+0 mut gives result 0, calculator gives: "+sum+", toll works!");
+        }else {
+            System.out.println(" add operation for 0+0 mut gives result 0, calculator gives: "+sum+", toll error!");
+        }
+
+        //test for Calculator.subtract
+        Calculator calculatorSubtract = new Calculator();
+        int difference = calculatorSubtract.subtract(0,0);
+        if (difference == 0){
+            System.out.println(" add operation for 0-0 mut gives result 0, calculator gives: "+difference+", toll works!");
+        }else {
+            System.out.println(" subtract operation for 0-0 mut gives result 0, calculator gives: "+difference+", toll error!");
+        }
     }
 }
