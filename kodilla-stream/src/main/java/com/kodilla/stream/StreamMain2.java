ada851f82d8761c0b96930672ed2bbf043094c67
SlawomirJablonski <forest19@interia.pl>
2018-10-22 20:15:34 +0200

    added ex7.3

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain2.java
@@ -0,0 +1,24 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamMain2 {
+    public static void main(String[] args) {
+        Forum forum = new Forum();
+
+        Map<Integer, ForumUser> resultMapOfForumUsers =
+        forum.getForumUsersList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> forumUser.getDateOfBirth().isBefore(LocalDate.now().minusYears(20)))
+                .filter(forumUser -> forumUser.getPostNumber() >= 1)
+                .collect(Collectors.toMap(forumUser -> forumUser.getUserId(), forumUser -> forumUser));
+
+        resultMapOfForumUsers.entrySet().stream()
+                .forEach(System.out::println);
+    }
+}
