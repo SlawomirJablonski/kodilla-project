84c0ddd73ea4174673521e08ddb945a478340138
SlawomirJablonski <forest19@interia.pl>
2018-10-23 14:23:48 +0200

    added ex7.2 v001

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.stream.beautifier;
-
-public class PoemBeautifier {
-    public String beautify(String text,PoemDecorator poemDecorator){
-        String decoratedText = poemDecorator.decorate(text);
-        return decoratedText;
-    }
-}
