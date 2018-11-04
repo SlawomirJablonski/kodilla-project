5981e82eb4da1d83c0f185b4132f9c868638f3d5
SlawomirJablonski <forest19@interia.pl>
2018-11-04 20:29:11 +0100

    added ex8.3

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,11 @@
+package com.kodilla.exception.test;
+public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge secondChallenge = new SecondChallenge();
+        try {
+            System.out.println(secondChallenge.probablyIWillThrowException(1.1, 1.6));
+        } catch (SecondChallengeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
