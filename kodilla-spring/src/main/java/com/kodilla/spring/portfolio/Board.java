92001e24a7931caf37cb687416d21b22e7a95d20
SlawomirJablonski <forest19@interia.pl>
2018-12-13 18:47:23 +0100

    ex14.3 v003 added Component to Board and refactoring in BoardConfig

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/Board.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-
 public class Board {
     private TaskList toDoList;
     private TaskList inProgressList;
